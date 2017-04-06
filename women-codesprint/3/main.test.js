eb9b3bfc35ee239346e16439f377b774415e5b33
Garance Vallat <garance.vallat@netcentric.biz>
2017-04-06 09:43:04 +0200

    women codesprint

---
+++ women-codesprint/3/main.test.js
@@ -0,0 +1,10 @@
+const main = require('./main');
+
+describe('Hackathon tShirts', () => {
+    describe(`Sample input`, () => {
+        test(`If ${minus} is always <`, () => {
+          expect(main.tShirts(5, 3, [0, 0, 1])).toBe(0);
+        });
+
+    });
+});
